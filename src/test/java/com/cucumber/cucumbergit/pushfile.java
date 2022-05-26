88c783a651d57e53a1709effefe8dbb686cf6096
lalithacivil <lalithacivil2015@gmail.com>
2022-05-26 15:31:52 +0530

    first time push

---
+++ src/test/java/com/cucumber/cucumbergit/pushfile.java
@@ -0,0 +1,17 @@
+package com.cucumber.cucumbergit;
+
+public class pushfile {
+	
+
+	public static void method1() {
+		System.out.println("Method1");
+	}
+		public static void method2() {
+		System.out.println("Method2");
+		}
+		public static void method3() {
+		System.out.println("Method3");	
+	}
+		
+	}
+
